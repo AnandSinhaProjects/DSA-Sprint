a441982d092d62be76f02a461376f8b41668badb
“AnandSProjects” <anands.projects22@gmail.com>
2022-09-26 22:21:53 +0530

    New Elements Added

--- Code-Notes/Lec 1/birthday_example.py
+++ Code-Notes/Lec 1/birthday_example.py
@@ -1,8 +1,16 @@
-import array as arr 
+class static_array:
+    def __init__(self, n):
+        self.data = [None] * n
+    def get_at(self,i):
+        if not (0<=i<len(self.data)): raise IndexError
+        return self.data[i]
+    def set_at(self,i,x):
+        if not (0<=i<len(self.data)): raise IndexError
+        self.data[i] = x
 
 def birthday_match(students) :
     n = len(students)
-    record = arr.Array(n)
+    record = static_array(n)
     for k in range(n):
         (name1, bday1) = students[k]
         for i in range(k):
@@ -12,4 +20,3 @@ def birthday_match(students) :
         record.set_at(k, (name1, bday1))
     return None
                 
-    
